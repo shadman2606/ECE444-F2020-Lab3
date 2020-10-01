71aeffafeb12ca4b9ff36509dc609186826e2bf7
Shadman Siddiqui <shadman2606@gmail.com>
2020-10-01 17:26:28 -0400

    Template with Bootstrap

---
+++ hello.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+boostrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    #return '<h1>Hello World!<h1>'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    #return '<h1>Hello, %s!<h1>' % name
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
