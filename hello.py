6a411aae454bfe90391f61830a0431529d34150d
Shadman Siddiqui <shadman2606@gmail.com>
2020-10-02 18:23:17 -0400

    Add templates, bootstrap, error pages, icon, time

--- hello.py
+++ hello.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 boostrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    #return '<h1>Hello World!<h1>'
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    #return '<h1>Hello, %s!<h1>' % name
     return render_template('user.html', name=name)
 
 @app.errorhandler(404)
