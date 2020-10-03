44595b2fb39b764654fb3db750b1484a09c5a93a
Shadman Siddiqui <shadman2606@gmail.com>
2020-10-02 22:35:07 -0400

    Add email input to form

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, Email
 
 
 app = Flask(__name__)
@@ -15,18 +15,32 @@ moment = Moment(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
+    email = StringField('What is your email?', validators=[Required(),Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET','POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        # check for new name and email
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        # check for UofT email
+        if 'utoronto' in form.email.data:
+            session['non_uoft_email'] = False
+        else:
+           session['non_uoft_email'] = True
+        session['email'] = form.email.data
+
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'))
+    return render_template('index.html', form = form, name = session.get('name'), 
+            email=session.get('email'), non_uoft_email=session.get('non_uoft_email'))
 
 @app.route('/user/<name>')
 def user(name):
